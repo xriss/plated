6839f520f78a4b3401a4aa450b3936ad6275dddf
kriss <kriss@xixs.com>
2019-03-02 03:36:52 +0000

    .

--- live/js/plated_live.js
+++ live/js/plated_live.js
@@ -1,6 +1,21 @@
 
 var plated_live=exports;
 
+var pify=require("pify")
 var git=require("isomorphic-git")
-var fs=require("@isomorphic-git/lightning-fs")
 
+
+var test=async function(){
+	
+	plated_live.fs=new (require("@isomorphic-git/lightning-fs"))('plated')
+	plated_live.pfs=pify(plated_live.fs)
+
+	console.log(plated_live.pfs)
+
+	var t = await plated_live.pfs.readdir("/");
+
+	console.log( t )
+
+}
+
+test()
