50d2942fc648df085304272a41370931c99d1c97
kriss <kriss@xixs.com>
2019-03-04 21:58:23 +0000

    menu bar

--- live/js/plated_live.js
+++ live/js/plated_live.js
@@ -47,8 +47,13 @@ plated_live.start=function(){
 		"lib/jquery-ui-themes/themes/ui-darkness/jquery-ui.min.css",
 		"lib/jquery.splitter.js",
 		"lib/jquery.splitter.css",
+		"lib/jquery.terminal.min.js",
+		"lib/jquery.terminal.min.css",
 	],function(){$(plated_live.start_loaded)})
 }
+
+plated_live.cmds={}
+plated_live.cmds.add=function(a,b){ this.echo(a + b); }
 
 plated_live.start_loaded=async function(){
 	
@@ -58,16 +63,35 @@ plated_live.start_loaded=async function(){
 	var resize_timeout;
 	var resize_func=function(event) {
 		var f=function() {
+			$("#split").height("100%")
+			$("#split_left").height("100%")
+			$("#split_right").height("100%")
 			window.dispatchEvent(new Event('resize'));
 		};
 		clearTimeout(resize_timeout);
 		timresize_timeouteout=setTimeout(f,100);
 	};
-	$( window ).resize(function() { $("#split").height("100%") }) // keep height full
+	$( window ).resize(resize_func) // keep height full
 	$("#split").height("100%").split({orientation:'vertical',limit:5,position:'15%',onDrag: resize_func });
+	$("#split_left").split({orientation:'horizontal',limit:5,position:'90%',onDrag: resize_func });
 //	$("#split_right").split({orientation:'horizontal',limit:5,position:'90%',onDrag: resize_func });
 
+	$('#menubar').menu({
+		position: { my: 'left top', at: 'left bottom' },
+		blur: function() {
+			$(this).menu('option', 'position', { my: 'left top', at: 'left bottom' });
+		},
+		focus: function(e, ui) {
+			if ($('#menubar').get(0) !== $(ui).get(0).item.parent().get(0)) {
+				$(this).menu('option', 'position', { my: 'left top', at: 'right top' });
+			}
+		},
+	});
 
+	plated_live.terminal=$('#pagecake_console').terminal(plated_live.cmds,{prompt:"^> "});
+
+	plated_live.terminal.echo("Welcome to the world of tomorrow!")
+	
 	plated_live.editor = ace.edit("editor");
 	plated_live.editor.setTheme("ace/theme/twilight");
 	plated_live.editor.session.setMode("ace/mode/javascript");
