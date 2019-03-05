d18f89e2c7f7541d86d717e3272c79f332ace881
kriss <kriss@xixs.com>
2019-03-05 15:17:55 +0000

    .

--- live/js/plated_live.js
+++ live/js/plated_live.js
@@ -11,25 +11,41 @@ plated.chunks.fill_chunks( require('fs').readFileSync(__dirname + '/chunks.css',
 
 plated.plate=function(str){ return plated.chunks.replace(str,plated_live.chunks) }
 
+plated_live.git_setup=async function(){
+	var fs=new (require("@isomorphic-git/lightning-fs"))('plated',{})
+	plated_live.git=require("isomorphic-git")
+	plated_live.git.plugins.set('fs', fs)
+	plated_live.pfs=(require("pify"))(fs)
+
+	plated_live.emitter = new (require('events'))
+	plated_live.git.plugins.set('emitter', plated_live.emitter)
+	plated_live.emitter.on('message', message => {
+		console.log(message)
+	})
+
+}
+
 plated_live.worker=async function(){
 
 	var MagicPortal=require("magic-portal")
 	var portal = new MagicPortal(self);
 
-	var git=require("isomorphic-git")
-
-	var fs=new (require("@isomorphic-git/lightning-fs"))('plated',{wipe:true})
-	var pfs=(require("pify"))(fs)
+	plated_live.git_setup()
 	
-	git.plugins.set('fs', fs)
+	portal.set('git', plated_live.git)
+	portal.set('pfs', plated_live.pfs)
+//	portal.set('emitter', plated_live.emitter)
 
-	portal.set('git', git)
-	portal.set('pfs', pfs)
 
 }
 
+plated_live.opts={}
 
-plated_live.start=function(){
+plated_live.opts.noworker=false
+
+plated_live.start=function(opts){
+	plated_live.opts=plated_live.opts || opts
+	
 	loadjs([
 		"lib/jquery.min.js",
 		"lib/jquery-ui/jquery-ui.min.js",
@@ -86,27 +102,46 @@ plated_live.start_loaded=async function(){
 	plated_live.editor.setTheme("ace/theme/twilight");
 	plated_live.editor.session.setMode("ace/mode/javascript");
 
-	var MagicPortal=require("magic-portal")
-	var worker = new Worker("lib/plated_live_worker.js")
-	plated_live.portal = new MagicPortal(worker)
+	if(plated_live.opts.noworker)
+	{
+		plated_live.git_setup()
+	}
+	else
+	{
+		var MagicPortal=require("magic-portal")
+		var worker = new Worker("lib/plated_live_worker.js")
+		plated_live.portal = new MagicPortal(worker)
 
-	plated_live.git = await plated_live.portal.get('git')
-	plated_live.pfs = await plated_live.portal.get('pfs')
+		plated_live.git = await plated_live.portal.get('git')
+		plated_live.pfs = await plated_live.portal.get('pfs')
+	}
 
 //	$( plated.plate("{dialogue_test}") ).dialog();
 
-	console.log( "fetching git" )
-	await plated_live.git.clone({
-		dir: '/',
-		corsProxy: 'https://cors.isomorphic-git.org',
-		url: 'https://github.com/xriss/plated-example.git',
-		ref: 'master',
-		singleBranch: true,
-//		depth: 10
-	})
+	plated_live.git_clone()
 
 	var t2 = await plated_live.pfs.readdir("/");
 	console.log( t2 )
 
+}
+
+plated_live.git_clone=async function(it){
+	it=it || {}
+	it.url=it.url || "https://github.com/xriss/"
+	it.name=it.name || "plated-example"
+
+	console.log( "fetching git" )
+	await plated_live.pfs.mkdir("/"+it.name).catch(error=>{
+		console.log(error); // ignore error
+	});
+	await plated_live.git.clone({
+		dir: '/'+it.name,
+		corsProxy: 'https://cors.isomorphic-git.org',
+		url: it.url+it.name,
+		ref: 'master',
+		singleBranch: true,
+		depth: 1
+	})
+	console.log( "fetched git" )
 }
 
